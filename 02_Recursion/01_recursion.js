ae74f535185612f3d574451a7c333e5405547b77
Abhay kumar <abhayrobotics1908@gmail.com>
2025-08-19 00:49:21 +0530

    recursion

--- 02_Recursion/01_recursion.js
+++ 02_Recursion/01_recursion.js
@@ -10,7 +10,7 @@ function fun(n){
     fun(n)
 }
 
-fun(20)
+// fun(20)
 
 
 function fun2(n,start){
@@ -23,4 +23,28 @@ function fun2(n,start){
     fun2(n,start)
 }
 
-fun2(20,1)
+// fun2(20,1)
+
+
+// TODO 29: Sum of 1st n nos using recursion
+
+function sum(n,total){
+    if(n==0){
+        console.log(total)
+        return ;
+    }
+    total =total+ n;
+    n--
+    sum(n,total)
+}
+
+sum(5,0)
+
+// better syntax
+function sum2(n){
+    if(n==0){
+        return 0;
+    }
+    return n+sum2(n-1)
+}
+console.log(sum2(5))
